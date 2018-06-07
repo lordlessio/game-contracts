9a8c38743578fbaf43f38688c87b805fb818555f
netpi <moon@netpi.me>
2018-06-07 17:50:57 +0800

    :[dev]

--- migrations/2_deploy_ldb.js
+++ migrations/2_deploy_ldb.js
@@ -1,5 +1,6 @@
 const LdbNFT = artifacts.require('./LdbNFT.sol');
-
+const fs = require('fs');
+const dbPath = require('path').join(require('os').homedir(), '.lordlessenv');
 module.exports = async function (deployer, network, accounts) {
   await liveDeploy(deployer, accounts);
 };
@@ -7,5 +8,12 @@ module.exports = async function (deployer, network, accounts) {
 async function liveDeploy (deployer, accounts) {
   const name = 'LDB NFT';
   const symbol = 'LDB';
-  await deployer.deploy(LdbNFT, name, symbol);
+  // await deployer.deploy(LdbNFT, name, symbol);
+  
+  this.ldbNFT = await LdbNFT.new(name, symbol, { from: accounts[0] });
+  const content = {
+    address: this.ldbNFT.address,
+    abi: this.ldbNFT.abi,
+  };
+  fs.writeFileSync(dbPath, JSON.stringify(content));
 }
